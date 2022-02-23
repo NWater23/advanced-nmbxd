aaff9644c58017fbcac1a28707a0ef5bbcab707a
Xu Siyuan <inqb@protonmail.com>
2022-02-23 10:38:08 +0800

    ver(0.4.9)

--- Advancednmb.user.js
+++ Advancednmb.user.js
@@ -12,7 +12,7 @@
 // @match       https://adnmb3.com/*
 // @require     https://code.jquery.com/jquery-2.2.4.min.js
 // @license     Apache License, Version 2.0 (Apache-2.0); https://opensource.org/licenses/Apache-2.0
-// @version     0.4.8
+// @version     0.4.9
 // @author      no1xsyzy
 // @grant       GM_setValue
 // @grant       GM_getValue
