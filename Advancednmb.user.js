97e25e22202c79212e4c76c35f520e7fd225b047
Xu Siyuan <inqb@protonmail.com>
2022-07-14 18:13:50 +0800

    bumpver

--- Advancednmb.user.js
+++ Advancednmb.user.js
@@ -6,7 +6,7 @@
 // @match       https://www.nmbxd1.com/*
 // @require     https://code.jquery.com/jquery-2.2.4.min.js
 // @license     Apache License, Version 2.0 (Apache-2.0); https://opensource.org/licenses/Apache-2.0
-// @version     0.5.0
+// @version     0.6.0
 // @author      no1xsyzy
 // @grant       GM_setValue
 // @grant       GM_getValue
